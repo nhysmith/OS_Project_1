167f9e7e4741e995504302fd01d0dfa609ab8f93
nhysmith <nickyhysmith@gmail.com>
2021-10-05 12:55:11 -0700

    Update Condition2.java

--- Condition2.java
+++ Condition2.java
@@ -30,11 +30,22 @@ public class Condition2 {
      * current thread must hold the associated lock. The thread will
      * automatically reacquire the lock before <tt>sleep()</tt> returns.
      */
-    public void sleep() {
+    
+    //wait()
+    @SuppressWarnings("static-access")
+	public void sleep() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
+	
+	//Enqueue thread to waitQ
+	waitQ.waitForAccess(KThread.currentThread());
+	
+	//Release the lock
 	conditionLock.release();
-
+	
+	//Suspend the thread
+	KThread.currentThread().sleep();
+	
+	//Acquire the lock
 	conditionLock.acquire();
     }
 
@@ -42,17 +53,46 @@ public class Condition2 {
      * Wake up at most one thread sleeping on this condition variable. The
      * current thread must hold the associated lock.
      */
+    
+    //signal()
     public void wake() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	
+	//If waitQ is not empty
+	if(waitQ.nextThread() != null)
+	{
+		//Dequeue the next process from waitQ
+		KThread temp = waitQ.nextThread();
+		
+		//Place next process in the scheduler's ready queue
+		temp.ready();
+		
+	}
+	
+	
     }
 
     /**
      * Wake up all threads sleeping on this condition variable. The current
      * thread must hold the associated lock.
      */
+    
+    //signalAll()
     public void wakeAll() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	
+	while(waitQ.nextThread() != null)
+	{
+		//Dequeue the next process from waitQ
+		KThread temp = waitQ.nextThread();
+		
+		//Place next process in the scheduler's ready queue
+		temp.ready();
+	}
+	
     }
 
     private Lock conditionLock;
+    
+    private ThreadQueue waitQ = ThreadedKernel.scheduler.newThreadQueue(true);
 }
